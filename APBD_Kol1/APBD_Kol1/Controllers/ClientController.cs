509adca33d84d54d7a1a742b42db0a2a7121328f
zyyzu2 <s24854@pjwstk.edu.pl>
2024-06-08 19:03:04 +0200

    controllerbase

--- APBD_Kol1/APBD_Kol1/Controllers/ClientController.cs
+++ APBD_Kol1/APBD_Kol1/Controllers/ClientController.cs
@@ -1,13 +1,12 @@
 ﻿using APBD_Kol1.Exceptions;
 using APBD_Kol1.Services;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace APBD_Kol1.Controllers;
 
 [Route("/api/client")]
-[ApiController]
-public class ClientController
+public class ClientController : ControllerBase
 {
     private readonly IClientService _service;
 
@@ -19,17 +18,17 @@ public class ClientController
     
     [Route("/{id}")]
     [HttpGet]
-    public async Task<IResult> getUserByID([FromRoute] int id)
+    public async Task<IActionResult> getUserByID([FromRoute] int id)
     {
         try
         {
             var result = await _service.getClientDetails(id);
-            return Results.Ok(result);
+            return Ok(result);
         }
         catch (NotFoundException e)
         {
 
-            return Results.NotFound(e.Message);
+            return NotFound(e.Message);
         }
     }
 }
--- APBD_Kol1/APBD_Kol1/Repositories/ClientRepository.cs
+++ APBD_Kol1/APBD_Kol1/Repositories/ClientRepository.cs
@@ -1,4 +1,6 @@
 ﻿using APBD_Kol1.Context;
+using APBD_Kol1.DTOs;
+using APBD_Kol1.Exceptions;
 using APBD_Kol1.Model;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,6 +8,7 @@ namespace APBD_Kol1.Repositories;
 public interface IClientRepository
 {
     public Task<Client?> GetClientById(int id);
+    public Task<ClientDetWithSubDTO> getClientById(int id);
 }
 
 public class ClientRepository : IClientRepository
@@ -21,5 +24,26 @@ public class ClientRepository : IClientRepository
     {
         var client = await _context.Clients.FirstOrDefaultAsync(s => s.IdClient == id);
         return client;
+    }
+
+    public async Task<ClientDetWithSubDTO> getClientById(int id)
+    {
+        var client = await _context.Clients.FirstOrDefaultAsync(s => s.IdClient == id);
+        if (client is null) throw new NotFoundException("Client not found");
+        var subs = await _context.Sales.Where(s => s.IdClient == client.IdClient).ToListAsync();
+        double sumprice = 0;
+        foreach (var sub in subs)
+        {
+            sumprice += sub.SubscriptionNavigation.Price;
+        }
+
+        var result = new ClientDetWithSubDTO()
+        {
+            FirstName = client.FirstName,
+            LastName = client.LastName,
+            Email = client.Email,
+        };
+        if (client.Phone is not null) result.Phone = client.Phone;
+        return result;
     }
 }
